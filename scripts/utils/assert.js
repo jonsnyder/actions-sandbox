c743ed4c2b00bdb406102fb4a953ff6d9e189258
Jon Snyder <josnyder@adobe.com>
2021-03-23 11:03:47 -0600

    Add export to assert util

--- scripts/utils/assert.js
+++ scripts/utils/assert.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const assert = (success, message) => {
+module.exports = (success, message) => {
   if (!success) {
     throw new Error(message);
   }
